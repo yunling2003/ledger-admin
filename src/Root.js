c9b509300ab8c56f4c89377d7c9ed0f3a8895f48
yunling2003 <yunling2003@aliyun.com>
2019-12-08 21:01:28 +0800

    feat: add layout page.

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import MainLayout from './Layout';
+import IssueList from './components/IssueList';
+
+export default function Root() {
+  return (
+    <Switch>
+      <MainLayout path="/" exact component={IssueList} />
+    </Switch>
+  );
+};
